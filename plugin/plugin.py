d7531f127ecacc7eee8c709e8761226ee01dddd5
kueken <ohnemailadresse@arcor.de>
2021-09-30 21:14:31 +0200

    cosmetic

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -2,10 +2,10 @@ from Plugins.Plugin import PluginDescriptor
 
 
 def autostart(reason, **kwargs):
-	from Plugins.SystemPlugins.ServiceMP3 import servicemp3
+    from Plugins.SystemPlugins.ServiceMP3 import servicemp3
 
 
 def Plugins(**kwargs):
-	return [
-		PluginDescriptor(where=PluginDescriptor.WHERE_AUTOSTART, needsRestart=True, fnc=autostart)
-	]
+    return [
+            PluginDescriptor(where=PluginDescriptor.WHERE_AUTOSTART, needsRestart=True, fnc=autostart)
+    ]
